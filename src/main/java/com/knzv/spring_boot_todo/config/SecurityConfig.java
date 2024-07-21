ddfa067e99c689927f01790bea6d55dfb0e1e254
Misha Knyazev <mdknyazev@yandex.ru>
2024-07-21 14:15:08 +0300

    refactoring

--- src/main/java/com/knzv/spring_boot_todo/config/SecurityConfig.java
+++ src/main/java/com/knzv/spring_boot_todo/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
